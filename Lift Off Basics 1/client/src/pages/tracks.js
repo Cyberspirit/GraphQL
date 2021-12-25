c4309e5a1f63048f190719b268c1d04a8aa3f88c
cyberspirit <ugochinyere.cyrine@gmail.com>
2021-12-25 18:04:31 +0100

    lift off basic part 2 clone

--- Lift Off Basics 1/client/src/pages/tracks.js
+++ Lift Off Basics 1/client/src/pages/tracks.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { Layout } from '../components';
-
+import React from "react";
+import { Layout } from "../components";
+import { gql, useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  if (loading) return <span>Loading...</span>;
+  if (error) return <span>Error Occured</span>;
+  if (data) console.log(data);
+  return (
+    <Layout grid>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </Layout>
+  );
 };
 
 export default Tracks;
